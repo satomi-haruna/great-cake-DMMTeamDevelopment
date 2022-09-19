2412b0cb8a10ba4bd6a36b1d55bcf2f09f00da8d
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-09-19 12:16:30 +0000

    [Add]配送先の登録、編集、更新、削除

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,39 +1,40 @@
 class Public::AddressesController < ApplicationController
-  
+
   def index
     @address = Address.new
     @addresses = Address.all
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     flash[:notice] = "You have created address successfully."
     redirect_to addresses_path
   end
 
   def update
-    @address = Address.find(prams[:id])
-    @Address.update(address_params)
-    redirect_to address_path(@address)
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
     flash[:notice] = "You have updated address successfully."
   end
 
   def destroy
     address = Address.find(params[:id])
     address.destroy
-    redirect_to address_path
+    redirect_to addresses_path
     flash[:notice] = "Address was successfully destroyed"
   end
-  
-  private
-  
+
+  protected
+
   def address_params
     params.require(:address).permit(:post_code, :address, :name)
   end
-  
+
 end
