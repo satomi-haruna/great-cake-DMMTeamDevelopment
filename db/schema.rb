caea1b058621a56b075ca6612ad52aafa936e8a1
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-19 10:37:04 +0000

    仮

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2022_09_18_062001) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "item_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
