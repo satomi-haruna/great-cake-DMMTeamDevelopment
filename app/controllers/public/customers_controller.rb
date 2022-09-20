fb2e55e5ba4a82c38f7cecc723ff6463e11ea32f
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-20 04:15:59 +0000

    [Add] my page edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,9 +4,19 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+    unless @customer == current_customer
+      redirect_to about_path
+    end
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :edit
+    end
   end
 
 
@@ -14,6 +24,7 @@ class Public::CustomersController < ApplicationController
 
     def set_customer
       @customer = Customer.find([:id])
+      redirect_to customers_my_page_path
     end
 
 end
