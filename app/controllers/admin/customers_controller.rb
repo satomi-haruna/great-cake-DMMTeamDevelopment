a67aa1383b3a1dc69ed8704e0af3dc3e89ff3f1d
MorioMurata <109211405+MorioMurata@users.noreply.github.com>
2022-09-21 07:53:31 +0000

    [finish]admin_customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "編集成功しました"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:telephone_number,:post_code,:address,:is_deleted)
   end
 
 end
