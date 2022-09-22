d1db04b1c02a4a3451f24c5c82a8d3eb43363957
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-09-22 09:14:15 +0000

    [Add]注文履歴一覧、注文履歴詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
     has_many :order_detail, dependent: :destroy
     
     enum payment_method: { credit_card: 0, transfer: 1 }
-    enum status: { 入金待ち: 0, 支払確認: 1, making: 2, ready_for_send: 3, sent: 4 }
+    enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
 end
