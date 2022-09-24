7df33d30cc23e9fe9fdc89106eb13556a1022ff9
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-09-24 11:41:22 +0000

    [Add]配送先、注文履歴詳細、注文履歴一覧のレイアウト

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,11 +5,13 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all.page(params[:page]).per(10)
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
 end
