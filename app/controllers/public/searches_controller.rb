92b3fdea7c7c820fe81596cb115a4c4deb98d6e2
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-09-24 07:07:35 +0000

    [Fix]コード修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,7 +5,7 @@ class Public::SearchesController < ApplicationController
     @model = params[:model]
     @content = params[:content]
     @method = params[:method]
-    if @model == 'customer'
+    if @model == 'current_customer'
           @records = Item.search_for(@content,@method)
     else  
           @records = Genre.search_for(@content,@method)
